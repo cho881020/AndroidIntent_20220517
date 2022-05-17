0145658149de960b1f58f4af3cff868e866cb376
조경진 <cho881020@gmail.com>
2022-05-17 10:43:13 +0900

    VIEW 액션 예제   - 웹링크 / 스토어 링크

--- app/src/main/java/kr/nepp/androidintent_20220517/MainActivity.kt
+++ app/src/main/java/kr/nepp/androidintent_20220517/MainActivity.kt
@@ -49,5 +49,22 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        btnNaverWebLink.setOnClickListener {
+
+            val myUri = Uri.parse("https://naver.com")
+            val myIntent = Intent( Intent.ACTION_VIEW, myUri )
+            startActivity(myIntent)
+
+        }
+
+        btnKakaoStoreLink.setOnClickListener {
+
+            val myUri = Uri.parse("market://details?id=com.kakao.talk")
+            val myIntent = Intent( Intent.ACTION_VIEW, myUri )
+            startActivity(myIntent)
+
+        }
+
+
     }
 }
